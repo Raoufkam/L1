a5e6f799227a3786f1f8233566d4720590ab6276
Kamiche Mohamed Raouf <93711354+Raoufkam@users.noreply.github.com>
2022-07-06 13:39:13 +0200

    Update DM2 info.py

--- Devoirs maison/DM2 info.py
+++ Devoirs maison/DM2 info.py
@@ -1,74 +1,9 @@
-Exo1)
-
-a)
-n est de type entier, car on utilise le int qui définit les nombres entiers
-b)
-j est de type entier, car dans le programme on voit bien qu'on fait une égalité
-entre j et n ( n est bien entier )
-c)
-i est de type liste , elle comporte les éléments entiers j donc i est une liste
-d'entiers j
-d)
-li est de type liste , elle comporte liste d'entiers i
-e)
-occ est une variable de type int ( entier ), car on lui ajoute 1 qui est bien un
-entier 
-f)
-Ce que le Programme devrait faire : Cherche a trouver le nombre de n dans la
-liste li , la liste li est formée de liste i  qui contient des valeurs égale
-à n , cette variable n elle prend la valeur dont on cherche le nombre d'apparitions
-( nombre de n dans la liste li ) , la variable occ compte le nombre de n
-il manque l'initialisation de la variable occ, donc on doit initialiser cette
-variable au début de notre programme pour qu'il fonctionne
-g)
-Exemple Représentatif :
-    On prend : li=[[4,8,9,2],[3,9,1,9]]
-               n=9
-               occ=0
-    alors on aura dans i,j et occ :
-             i=[4,8,9,2]
-             j=4
-             j=8
-             j=9      #occ=occ+1 ( car la condition est vraie )
-             j=2
-             occ=1
-             i=[3,9,1,9]
-             j=3
-             j=9      #occ=occ+1 ( car la condition est vraie )
-             j=1
-             j=9      #occ=occ+1 ( car la condition est vraie )
-             occ=3
-Exo2)
-
-#initialisation de la liste :
-liste=eval(input())
-#initialisation du nombre :
-nombre=int(input())
-#initialisation de occurence_nombre:
-occurence_nombre=0
-#pour chaque élément de 'liste','liste2' prend tous les éléments de 'liste':
-for liste2 in liste :
-    #pour chaque élément de 'liste2' , 'verif' prend tous les éléments de 'liste2':
-    for verif in liste2 :
-        #Condition pour vérifier si 'verif' est égale à 'nombre' :
-        if verif==nombre :
-            #Ajouter 1 a chaque fois que la Condition est Vraie ( verif==nombre):
-            occurence_nombre=occurence_nombre+1
-#Afficher le résultat de 'occurence_nombre :
-print(occurence_nombre)
-
-Exo3)
-
 #Programme qui calcul le nombre d’éléments divisibles par 5 de la variable 'li' :
-liste=eval(input())
+print("Faite entrer votre liste de nombres : ")
+liste =eval(input("Entrez votre liste : "))
 occurence_nombre=0
-for liste2 in liste :
-    for verif in liste2 :
-        if verif%5==0 :
-            occurence_nombre=occurence_nombre+1
-print(occurence_nombre)
-
-
-
-
-
+for elem in liste :
+    if elem%5==0 :
+        print("le nombre :",elem,"est divisible par 5")
+        occurence_nombre=occurence_nombre+1
+print("Le nombre d'occurence des nombres divisble par 5 est : ",occurence_nombre)
